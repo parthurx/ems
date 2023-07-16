5eec26f13006899c2d19b2ce4a7c27b6404e852e
unknown <parthurx@hotmail.com>
2023-07-16 10:50:56 -0300

    implemented all get routes of employees

--- ems-backend/src/main/java/com/paulo/ems/service/EmployeeService.java
+++ ems-backend/src/main/java/com/paulo/ems/service/EmployeeService.java
@@ -2,6 +2,12 @@ package com.paulo.ems.service;
 
 import com.paulo.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- ems-backend/src/main/java/com/paulo/ems/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/paulo/ems/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,15 @@ package com.paulo.ems.service.impl;
 
 import com.paulo.ems.dto.EmployeeDto;
 import com.paulo.ems.entity.Employee;
+import com.paulo.ems.exception.ResourceNotFoundException;
 import com.paulo.ems.mapper.EmployeeMapper;
 import com.paulo.ems.repository.EmployeeRepository;
 import com.paulo.ems.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +24,20 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Funcionário não existe com determinado id:" + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
