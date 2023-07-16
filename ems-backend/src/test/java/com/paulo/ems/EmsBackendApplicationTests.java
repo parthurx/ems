29bc5810ee71fb1c1d1e5463302c21683e52c2ce
unknown <parthurx@hotmail.com>
2023-07-16 07:26:31 -0300

    Connection with database

---
+++ ems-backend/src/test/java/com/paulo/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
